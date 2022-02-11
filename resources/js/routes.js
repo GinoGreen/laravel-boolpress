a8a34004c022e11ef00cc5347ae271617a395021
GinoGreen <luigiverde97@gmail.com>
2022-02-11 15:40:32 +0100

    routing pages

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,12 +1,30 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contacts from './components/pages/Contacts';
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
    mode: 'history',
+   
    routes: [
-      
+      {
+         path: '/',
+         name: 'home',
+         component: Home,
+      },
+      {
+         path: '/chi-siamo',
+         name: 'about',
+         component: About,
+      },
+      {
+         path: '/contatti',
+         name: 'contacts',
+         component: Contacts,
+      },
    ],
 });
 
